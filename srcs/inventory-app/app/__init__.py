3dc300550059983ce4d90087325f5943f9927b87
kill-ux <mustaphaboutoubdev@gmail.com>
2026-04-29 16:58:13 +0100

    get movie

--- srcs/inventory-app/app/__init__.py
+++ srcs/inventory-app/app/__init__.py
@@ -13,6 +13,7 @@ def create_app():
     from .routes.health import health_bp
 
     app.register_blueprint(movies_bp)
+    app.register_blueprint(health_bp)
 
     db.init_app(app)
     with app.app_context():
@@ -20,11 +21,11 @@ def create_app():
 
     @app.errorhandler(Exception)
     def handle_exception(e):
-        if hasattr(
-            e,
-            "code",
-        ):
-            return {"error": e.description}, e.code
+        if hasattr(e, "code"):
+            print(f"Error: {e}, Code: {e.code}")
+            if hasattr(e, "description"):
+                return {"error": e.description}, e.code
+            return {"error": "An error occurred"}, e.code
         return {"error": "Internal Server Error", "message": str(e)}, 500
 
     return app
--- srcs/inventory-app/app/routes/movies.py
+++ srcs/inventory-app/app/routes/movies.py
@@ -1,24 +1,44 @@
 from flask import Blueprint, request
 from ..models import Movie, db
 
-movies_bp = Blueprint('movies_bp', __name__, url_prefix='/api/movies')
+movies_bp = Blueprint("movies_bp", __name__, url_prefix="/api/movies")
 
-@movies_bp.route('/', methods=['GET'])
+
+@movies_bp.route("/", methods=["GET"])
 def list_movies():
-    search_query = request.args.get('title')
+    """List all movies"""
+
+    search_query = request.args.get("title")
     if search_query:
         movies = Movie.query.filter(Movie.title.ilike(f"%{search_query}%"))
     else:
         movies = Movie.query.all()
     return [movie.to_dict() for movie in movies]
 
-@movies_bp.route('/', methods=['POST'])
-def create_movie():
-    data = request.get_json()
-    if not data or 'title' not in data:
-        return {'error': 'Missing title'}, 400
 
-    movie = Movie(title=data['title'], description=data.get('description', ''))
+@movies_bp.route("/", methods=["POST"])
+def create_movie():
+    """Create a movie"""
+    
+    data = request.get_json()
+    if not data or "title" not in data:
+        return {"error": "Missing title"}, 400
+
+    movie = Movie(title=data["title"], description=data.get("description", ""))
     db.session.add(movie)
     db.session.commit()
     return movie.to_dict(), 201
+
+@movies_bp.route('/', methods=["DELETE"])
+def delete_movies():
+    """Delete All movies"""
+    Movie.query.delete()
+    db.session.commit()
+    return {"message": "All movies deleted"}, 200
+
+@movies_bp.route("/<int:id>", methods=["GET"])
+def get_movie(id):
+    movie = Movie.query.get(id)
+    if not movie:
+        return {"error": "Movie not found"}, 404
+    return movie.to_dict()
